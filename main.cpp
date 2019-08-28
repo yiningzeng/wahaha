3bb0f247246f0d565a0a7aaf56d11f7b705501b8
yiningzeng <yiningzeng@outlook.com>
2019-08-28 19:57:33 +0800

    feat: 重新实现，面积可能有问题

--- main.cpp
+++ main.cpp
@@ -53,31 +53,73 @@ int dodo(String imgpath){
     Canny(grayImg, cannyPic, cannyThr, cannyThr*FACTOR); //Canny边缘检测
     imshow("cannyPic", cannyPic);
 
-
-    vector<vector<Point>> contours;    //储存轮廓
+    CvMemStorage* storage = cvCreateMemStorage(0);
+    CvSeq* contours = NULL;//轮廓    //储存轮廓
     vector<Vec4i> hierarchy;
+    vector<Point> listPoint(4);
+//    findContours(cannyPic, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);    //获取轮廓
 
-    findContours(cannyPic, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);    //获取轮廓
+    IplImage imgTmp = cannyPic;//为了方便程序运行，这里采用老版的图像数据类型
+    IplImage  *src = cvCloneImage(&imgTmp);//图像数据的深拷贝
+    int count = cvFindContours(src, storage, &contours, \
+		sizeof(CvContour), CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);//找出各个连通域
+    CvBox2D box;//外接矩形
+    CvBox2D maxLenBox;//最长长条的外接矩形
 
-    vector<vector<Point>> polyContours(contours.size());
     Mat linePic = Mat::zeros(cannyPic.rows, cannyPic.cols, CV_8UC3);
-    int maxArea = 0;
-    int area = 0;
-    for (int index = 0; index < contours.size(); index++){
-        if (contourArea(contours[index]) > contourArea(contours[maxArea])){
-            maxArea = index;
-            area=contourArea(contours[index]);
-            approxPolyDP(contours[index], polyContours[index], 10, true);
+    for (; contours != 0; contours = contours->h_next)//找到最长的长条
+    {
+        double area = cvContourArea(contours);
+        if(area>13 && area <800){
+            box = cvMinAreaRect2(contours, 0);//调用cvMinAreaRect2()函数返回外接矩形给box
+            CvPoint2D32f rect_pts0[4];
+            cvBoxPoints(box, rect_pts0);
+            listPoint.push_back(rect_pts0[0]);
+            listPoint.push_back(rect_pts0[1]);
+            listPoint.push_back(rect_pts0[2]);
+            listPoint.push_back(rect_pts0[3]);
+            std::cout << "big area:" << area << std::endl;
         }
-        drawContours(linePic, contours, index, Scalar(rand() & 255, rand() & 255, rand() & 255), 1, 8/*, hierarchy*/);
-    }
-    std::cout << area << std::endl;
-    Mat polyPic = Mat::zeros(srcImage.size(), CV_8UC3);
-    drawContours(polyPic, polyContours, maxArea, Scalar(0,0,255/*rand() & 255, rand() & 255, rand() & 255*/), 2);
+        std::cout << area << std::endl;
 
-//    CvSeq* contour = NULL;//轮廓
-//    CvBox2D box;//外接矩形
-//    CvBox2D maxLenBox;//最长长条的外接矩形
+//        CvPoint2D32f* point2D32f;
+//        cvBoxPoints(box, point2D32f);
+//        listPoint.insert(point2D32f[0]);
+//        listPoint.Add(point2D32f[1]);
+//        listPoint.Add(point2D32f[2]);
+//        listPoint.Add(point2D32f[3]);
+        std::cout << area << std::endl;
+    }
+    RotatedRect rRect;
+    rRect = minAreaRect(listPoint);
+    Point2f vertices[4];
+    rRect.points(vertices);
+
+//    int x = (int)(rRect.center.x - Math.Min(box.size.width, box.size.height) / 2f) + iX;
+//    int y = (int)(rRect.center.y - Math.Max(box.size.width, box.size.height) / 2) + iY;
+
+    for (int i = 0; i < 4; i++)
+        line(srcImage, vertices[i], vertices[(i+1)%4], Scalar(0,255,0), 2);
+    imshow("final", srcImage);
+
+//    vector<vector<Point>> polyContours(contours.size());
+//    Mat linePic = Mat::zeros(cannyPic.rows, cannyPic.cols, CV_8UC3);
+//    int maxArea = 0;
+//    int area = 0;
+//    for (int index = 0; index < contours.size(); index++){
+//        if (contourArea(contours[index]) > contourArea(contours[maxArea])){
+//            maxArea = index;
+//            area=contourArea(contours[index]);
+//            approxPolyDP(contours[index], polyContours[index], 10, true);
+//        }
+//        cv::drawContours(linePic, contours, index, Scalar(rand() & 255, rand() & 255, rand() & 255), 1, 8/*, hierarchy*/);
+//    }
+//    std::cout << area << std::endl;
+//    Mat polyPic = Mat::zeros(srcImage.size(), CV_8UC3);
+//    drawContours(polyPic, polyContours, maxArea, Scalar(0,0,255/*rand() & 255, rand() & 255, rand() & 255*/), 2);
+//
+//
+//
 //    for (; contour != 0; contour = contour->h_next)//找到最长的长条
 //    {
 //        box = cvMinAreaRect2(contour, 0);//调用cvMinAreaRect2()函数返回外接矩形给box
@@ -99,7 +141,7 @@ int dodo(String imgpath){
 ////
 ////    drawContours(polyPic, contours, maxArea, Scalar(rand() & 255, rand() & 255, rand() & 255), 1, 8/*, hierarchy*/);
 
-    imshow("polyPic", polyPic);
+//    imshow("polyPic", polyPic);
     waitKey(0);
 }
 
